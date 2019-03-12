106ca058d8b44a0af4cf76adf4fcab9c8ed18d1f
Jake Bonham <jake@docforce.io>
2019-03-11 17:42:23 -0700

    Updated intro text

--- homepages/stateoftc/src/components/Intro.js
+++ homepages/stateoftc/src/components/Intro.js
@@ -3,13 +3,11 @@ import React from 'react'
 const Intro = () => (
     <div className="Block Block--intro Intro">
         <h2>Technical Communication is evolving faster than ever.</h2>
-        <p> Flexbox, Grid, Multi-Column… To say nothing of whole new paradigms like CSS-in-JS.</p>
-        <p>
-            So after the success of our annual{' '}
-            <a href="https://stateofjs.com">State Of JavaScript</a> survey, we’ve decided to take on
-            the world of styles and selectors to help identify the latests trends… and try to bridge
-            the ever-growing “great front-end divide”!
-        </p>
+
+        <p>The goal of technical communicators is to make information more useable and accessible to those who need that information.</p>
+
+        <p>We have put this survey together to try and sort out the latest trends and where things can improve. </p>
+
     </div>
 )
 
